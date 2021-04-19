d243ae2e32990a4c86f96d43d2934a593665e03c
nyamburanjuguna <njugunajoan35@gmail.com>
2021-04-19 15:40:03 +0300

    create: User model with Authentication Token

--- Gemfile
+++ Gemfile
@@ -3,6 +3,14 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.0'
 
+#
+##  Added Gems
+#
+
+# Devise is a flexible authentication solution for Rails based on Warden
+gem 'devise', '~> 4.2'
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.1'
 # Use sqlite3 as the database for Active Record
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate_user_from_token!
+    before_action :authenticate_user!
+
+
+    private
+  
+    # For this example, we are simply using token authentication
+    # via parameters. However, anyone could use Rails's token
+    # authentication features to get the token from a header.
+    # def authenticate_user_from_token!
+    #     user_token = params[:user_token].presence
+    #     user       = user_token && User.find_by_authentication_token(user_token.to_s)
+
+    #     if user
+    #     # Notice we are passing store false, so the user is not
+    #     # actually stored in the session and a token is needed
+    #     # for every request. If you want the token to work as a
+    #     # sign in token, you can simply remove store: false.
+    #     sign_in user, store: false
+    #     end
+    # end
+    def authenticate_user_from_token!
+        auth_token = params[Devise.token_authentication_key]
+        if auth_token
+          t = Time.now
+          if (user = User.where(authentication_token: auth_token).first)
+            sign_in user, store: false
+          else
+            # ensure requests with a failed token match are quantised to 200ms
+            sleep((200 - Time.now + t) % 200) / 1000.0)
+          end
+        end
+    end
 end
