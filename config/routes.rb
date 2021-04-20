cb7900bad57a4257fd3834064e0f909993c13448
nyamburanjuguna <njugunajoan35@gmail.com>
2021-04-20 14:17:49 +0300

    create: Sign In API

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do 
       devise_scope :user do 
         post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
       end
     end 
   end
